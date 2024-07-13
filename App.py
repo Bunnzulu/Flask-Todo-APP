9f09aa793c74b7503efe48f6e51f9d8fea0bde4e
Nobody <bunnza2020@gmail.com>
2024-07-13 14:48:02 -0500

    Update App.py

--- App.py
+++ App.py
@@ -5,3 +5,11 @@ app = Flask(__name__ ,template_folder= "Templates")
 @app.route('/')  #Basically what shows on home page
 def hello_World():
     return "Hello World"
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
