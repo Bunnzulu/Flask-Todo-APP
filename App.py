2100b94f481c5519ff8ece424660371d21111800
Nobody <bunnza2020@gmail.com>
2024-07-15 09:40:49 -0500

    Sign-up Screen

--- App.py
+++ App.py
@@ -2,11 +2,14 @@ from flask import Flask,render_template
 
 app = Flask(__name__ ,template_folder= "Templates")
 
-@app.route('/')  #Basically what shows on home page
+@app.route('/',methods=["post"])  #Basically what shows on home page
 def Login_Screen():
     return render_template("Login_Screen.html")
 
 
+@app.route('/Sign-up')  
+def Sign_Up_Screen():
+    return render_template("Sign_up.html")
 
 
 
