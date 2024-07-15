89cb5b7fb9af44bced31a3acf8143befc60d9a10
Nobody <bunnza2020@gmail.com>
2024-07-15 13:02:38 -0500

    Check UserNames

--- App.py
+++ App.py
@@ -1,23 +1,29 @@
 from flask import Flask,render_template,request
-from Database import Add_Account
+from Database import Add_Account,Get_Accounts
 
 app = Flask(__name__ ,template_folder= "Templates")
-
+ACCOUNTS = Get_Accounts()
 @app.route('/')  #Basically what shows on home page
 def Login_Screen():
-    return render_template("Login_Screen.html")
+    return render_template("Login_Screen.html",Accounts=ACCOUNTS)
 
 
 @app.route('/Sign-up')  
 def Sign_Up_Screen():
+    UserNames = [name["username"] for name in ACCOUNTS]
+    with open("UserNames.txt","w") as file:
+        for Name in UserNames: 
+            file.write(Name)
+            file.write("\n")
     return render_template("Sign_up.html")
+
 
 @app.route('/Signed-up',methods=["post"])  
 def Signed_Up_Screen():
     data = request.form
-    Add_Account(data)
-    return render_template("Sign_up.html")
-
+    New_account = Add_Account(data)
+    if New_account:return render_template("ConfirmSign-in.html")
+    else:return render_template("BadUserName.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Database.py
+++ Database.py
@@ -7,9 +7,23 @@ connection = os.getenv("DB_CONNECTION_STRING")
 
 engine = sqlal.create_engine(connection, connect_args={'connect_timeout': 60})
 
-
 def Add_Account(data):
+    file = open("UserNames.txt","r")
+    words = file.read()
+    UserNames = words.split("\n")
+    file.close()   
+    if data.get("Username") not in UserNames:
+        with engine.connect() as conn:
+            query = sqlal.text("INSERT INTO Accounts (username, password) VALUES (:username, :password)")
+            conn.execute(query,{"username":data.get("Username"),"password":data.get("Password")})
+            conn.commit()
+        return True
+    else:return False
+
+def Get_Accounts():
     with engine.connect() as conn:
-        query = sqlal.text("INSERT INTO Accounts (username, password) VALUES (:username, :password)")
-        conn.execute(query,{"username":data.get("Username"),"password":data.get("Password")})
-        conn.commit()
+        result = conn.execute(sqlal.text("select * from Accounts"))
+        Accounts = []
+        for row in result.all():
+            Accounts.append(dict(row._mapping))
+        return Accounts
