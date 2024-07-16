ee80497cbc7c6b9915e521d7deb48656a5e2d0e7
Nobody <bunnza2020@gmail.com>
2024-07-16 12:36:53 -0500

    Revert "n"
    This reverts commit f4de0a313bd760cf8eadb00d0b95ebbcca348223.

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 from Database import Add_Account,Get_Accounts,Verify_Cred,Add_to_Notes,Show_Notes,Delete_Note
 
-app = Flask(__name__ )
+app = Flask(__name__ ,template_folder= "Templates")
 ACCOUNTS = Get_Accounts()
 Login = [False,""]
 
